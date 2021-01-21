ba3ab2264f497ba506f238769056b896379e5282
YutaSugimura <nextc3m@gmail.com>
2021-01-21 23:06:22 +0900

    Create lightweight template

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ['./pages/**/*.js', './components/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    fontFamily: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
