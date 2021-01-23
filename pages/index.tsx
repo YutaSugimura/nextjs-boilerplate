da0086013d4f5f7af98b989c4b2b70c9389fa855
YutaSugimura <nextc3m@gmail.com>
2021-01-23 23:42:34 +0900

    Add storybook\

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,8 @@ const Page: React.VFC<Props> = () => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
@@ -31,7 +32,10 @@ const Page: React.VFC<Props> = () => {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -41,7 +45,9 @@ const Page: React.VFC<Props> = () => {
             className={styles.card}
           >
             <h3>Deploy &rarr;</h3>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
           </a>
         </div>
       </main>
@@ -52,7 +58,8 @@ const Page: React.VFC<Props> = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
