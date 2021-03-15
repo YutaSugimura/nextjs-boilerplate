a73ae37a8c57ea1261f66a86e68d65bc7184b459
YutaSugimura <nextc3m@gmail.com>
2021-03-15 23:17:57 +0900

    Add mocks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import type { AppProps /*, AppContext */ } from 'next/app';
-import "tailwindcss/tailwind.css";
+import 'tailwindcss/tailwind.css';
+
+if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
+  require('../.mocks');
+}
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
+  return <Component {...pageProps} />;
+};
 
 export default App;
