f7978c676343d941e358181f3a56e0b8d8edc641
YutaSugimura <nextc3m@gmail.com>
2021-05-31 23:45:04 +0900

    upgrade next.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  future: {
-    webpack5: true,
-  },
   env: {
     NEXT_PUBLIC_API_MOCKING: 'enabled',
   },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { Card } from '../src/components/molecules/card';
+import { Card } from '../components/molecules/card';
 
 type Props = {
   data: {
@@ -34,8 +34,7 @@ const Page: React.VFC<Props> = ({ data }) => {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Get started by editing <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
@@ -49,10 +48,7 @@ const Page: React.VFC<Props> = ({ data }) => {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
@@ -62,9 +58,7 @@ const Page: React.VFC<Props> = ({ data }) => {
             className={styles.card}
           >
             <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
 
@@ -82,8 +76,7 @@ const Page: React.VFC<Props> = ({ data }) => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
