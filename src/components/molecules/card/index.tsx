7382a83f092a8ccc23c1ae88af37d44cd853aba5
YutaSugimura <nextc3m@gmail.com>
2021-07-07 16:44:35 +0900

    fix

---
+++ src/components/molecules/card/index.tsx
@@ -0,0 +1,19 @@
+import useSWR from 'swr';
+
+type Props = {};
+
+export const Card: React.VFC<Props> = () => {
+  const { data, error } = useSWR<{ title: string; text: string }>('csr', () =>
+    fetch('https://myapi.dev/csr').then((res) => res.json()),
+  );
+
+  if (error) return <p>error</p>;
+  if (!data) return <p>...loading</p>;
+
+  return (
+    <div>
+      <p>{data.title}</p>
+      <p>{data.text}</p>
+    </div>
+  );
+};
