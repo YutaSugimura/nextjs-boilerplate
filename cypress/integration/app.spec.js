88dd55a19f44c5cd269605418dcc4c34da1f9886
YutaSugimura <nextc3m@gmail.com>
2021-09-09 23:09:13 +0900

    fix: tests

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -3,10 +3,25 @@
 // If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
 // https://github.com/cypress-io/eslint-plugin-cypress
 
-describe('e2e Test', () => {
-  it('should h1 content to the home page', () => {
-    cy.visit('http://localhost:3000/home');
+describe('Top Page e2e Testing', () => {
+  it('should h1 content to the index page', () => {
+    cy.visit('http://localhost:3000');
 
-    cy.get('h1').contains('Home');
+    cy.get('h1').contains('Welcome to Next.js!');
+  });
+
+  it('should h3 in the box to the index page', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.get(`a[href*="https://nextjs.org/docs"]`).get('h3').contains('Documentation →');
+    cy.get('a[href*="https://nextjs.org/learn"]').get('h3').contains('Learn →');
+    cy.get('a[href*="https://github.com/vercel/next.js/tree/master/examples"]')
+      .get('h3')
+      .contains('Examples →');
+    cy.get(
+      'a[href*="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"]',
+    )
+      .get('h3')
+      .contains('Deploy →');
   });
 });
