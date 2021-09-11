e635bce22e5a1a1b21a25ac8a068b87ce07235ad
YutaSugimura <nextc3m@gmail.com>
2021-09-11 12:07:50 +0900

    upgrade: tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   mode: 'jit',
-  purge: [
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-  ],
+  purge: {
+    content: [
+      './src/components/**/*.{js,ts,jsx,tsx}',
+      './src/pages/**/*.{js,ts,jsx,tsx}',
+    ],
+    safelist: [],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
